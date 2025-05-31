195d4791c4445ac95429199d2f4945e25b2a5c87
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-05-30 20:16:01 -0400

    File controller and routes

--- server/src/controllers/fileController.ts
+++ server/src/controllers/fileController.ts
@@ -5,7 +5,6 @@ import { User } from '../../generated/prisma';
 
 export const createFile = asyncHandler(async (req, res) => {
   const user = req.user as User;
-  const { name: customName, folderId } = req.body;
   if (!req.file) {
     res.status(400).json({ error: 'No file uploaded' });
     return;
@@ -14,8 +13,10 @@ export const createFile = asyncHandler(async (req, res) => {
   const file = req.file;
   const bucket = 'uploads';
   const extension = file.originalname.split('.')[1];
-  const name = customName ? `${customName}.${extension}` : file.originalname;
-  const path = `${user.id}/${folderId ?? 'home'}/${name}`;
+  const name = req.body.name
+    ? `${req.body.name}.${extension}`
+    : file.originalname;
+  const path = `${user.id}/${req.body.folderId ?? 'home'}/${name}`;
 
   const { data, error } = await supabase.storage
     .from(bucket)
@@ -26,14 +27,85 @@ export const createFile = asyncHandler(async (req, res) => {
     return;
   }
 
-  const prismaFile = await db.createFile({
-    name,
-    owner: { connect: { id: user.id } },
-    mimeType: file.mimetype,
-    path,
-    bucket,
-    size: file.size,
-    ...(folderId && { folder: { connect: { id: folderId } } }),
-  });
-  res.status(200).json(prismaFile);
+  try {
+    const prismaFile = await db.createFile({
+      name,
+      owner: { connect: { id: user.id } },
+      mimeType: file.mimetype,
+      path,
+      bucket,
+      size: file.size,
+      ...(req.body.folderId && {
+        folder: { connect: { id: req.body.folderId } },
+      }),
+    });
+    res.status(200).json(prismaFile);
+  } catch (err) {
+    console.error(err);
+    const { data, error } = await supabase.storage.from(bucket).remove([path]);
+  }
+});
+
+export const deleteFile = asyncHandler(async (req, res) => {
+  const { fileId } = req.params;
+  if (!fileId) {
+    res
+      .status(400)
+      .json({ status: 'fail', message: 'Provide fileId to delete file.' });
+    return;
+  }
+  if (!(await db.fileExists(Number(fileId)))) {
+    res
+      .status(404)
+      .json({ status: 'fail', message: 'fileId does not exists.' });
+    return;
+  }
+
+  const file = await db.deleteFile(Number(fileId));
+
+  const { data, error } = await supabase.storage
+    .from(file.bucket)
+    .remove([file.path]);
+  res.status(204).json(file);
+});
+
+export const updateFile = asyncHandler(async (req, res) => {
+  const { data } = req.body;
+  const { fileId } = req.params;
+  if (!fileId) {
+    res
+      .status(400)
+      .json({ status: 'fail', message: 'Provide fileId to delete file.' });
+    return;
+  }
+
+  const file = await db.updateFile(Number(fileId), data);
+  res.status(204).json(file);
+});
+
+export const getFileById = asyncHandler(async (req, res) => {
+  const { fileId } = req.params;
+  const file = await db.getFileById(Number(fileId));
+  if (!file) {
+    res.status(404).json({ status: 'fail', message: 'File not found' });
+    return;
+  }
+  res.status(200).json(file);
+});
+
+export const getAllFiles = asyncHandler(async (req, res) => {
+  let folderId;
+  if (!req.body?.folderId) {
+    folderId = null;
+  } else {
+    folderId = Number(req.body.folderId);
+  }
+
+  let files;
+  if (!folderId) {
+    files = await db.getAllFiles(folderId);
+  } else {
+    files = await db.getAllFiles(folderId);
+  }
+  res.status(200).json(files);
 });
--- server/src/routes/fileRouter.ts
+++ server/src/routes/fileRouter.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import { upload } from '../middleware/upload';
-import { createFile } from '../controllers/fileController';
+import {
+  createFile,
+  deleteFile,
+  getAllFiles,
+  getFileById,
+  updateFile,
+} from '../controllers/fileController';
 import { ensureAuthenticated } from '../controllers/authController';
 
 const fileRouter = Router();
@@ -11,5 +17,9 @@ fileRouter.post(
   upload.single('file'),
   createFile
 );
+fileRouter.delete('/:fileId', ensureAuthenticated, deleteFile);
+fileRouter.put('/:fileId', ensureAuthenticated, updateFile);
+fileRouter.get('/:fileId', ensureAuthenticated, getFileById);
+fileRouter.get('/', ensureAuthenticated, getAllFiles);
 
 export default fileRouter;

bb617f95f900e6a95c20aeed30bc56aa13f45be0
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-06-03 17:16:10 -0400

    User route and Controller

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,28 @@
+import db from '../db';
+import asyncHandler from 'express-async-handler';
+
+export const getUserById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await db.getUserById(Number(id));
+  if (!user) {
+    res
+      .status(400)
+      .json({ status: 'fail', message: 'There is no user with that id.' });
+    return;
+  }
+  res.status(200).json(user);
+});
+
+export const updateUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  const user = req.user;
+  if (!user) {
+    res
+      .status(400)
+      .json({ status: 'fail', message: 'There is no user with that id.' });
+    return;
+  }
+  const updatedUser = await db.updateUser(Number(id), data, true);
+  res.status(201).json({ updatedUser });
+});
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../controllers/authController';
+import { getUserById, updateUser } from '../controllers/userController';
 
 const userRouter = Router();
 
+userRouter.get('/:id', ensureAuthenticated, getUserById);
+userRouter.put('/:id', ensureAuthenticated, updateUser);
 export default userRouter;

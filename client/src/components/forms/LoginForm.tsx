f66a80ed4f4f9936d46409cd4a999e1d703a034b
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-06-19 19:37:33 -0400

    Login refactor

--- client/src/components/forms/LoginForm.tsx
+++ client/src/components/forms/LoginForm.tsx
@@ -1,43 +1,62 @@
 import { FcGoogle } from 'react-icons/fc';
-import { Button, Field, Fieldset, Input } from '@chakra-ui/react';
+import { Button, Field, Fieldset, Input, Box } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useAuth } from '@/hooks/useAuth';
 import { login } from '@/api/authApi';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const { login: contextLogin } = useAuth();
 
-  const handleSignin = async () => {
+  const handleSignin = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
     const user = await login(username, password);
     contextLogin(user);
   };
   return (
-    <Fieldset.Root size="lg" maxW="md">
-      <Field.Root>
-        <Field.Label id="username">Username: </Field.Label>
-        <Input
-          placeholder="Enter your username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </Field.Root>
+    <Box
+      as="form"
+      maxW="md"
+      minW="sm"
+      mx="auto"
+      p={8}
+      borderWidth={1}
+      borderRadius="lg"
+      bg="gray.subtle"
+      boxShadow="md"
+      onSubmit={handleSignin}
+    >
+      <Fieldset.Root size="lg" maxW="md">
+        <Fieldset.Legend fontSize="xl">Sign in to your account</Fieldset.Legend>
+        <Field.Root>
+          <Field.Label id="username">Username: </Field.Label>
+          <Input
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Field.Root>
 
-      <Field.Root>
-        <Field.Label id="password">Password: </Field.Label>
-        <Input
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Field.Root>
+        <Field.Root>
+          <Field.Label id="password">Password: </Field.Label>
+          <Input
+            placeholder="Enter your password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Field.Root>
 
-      <Button onClick={handleSignin}>Sign in</Button>
-      <Button>
-        Sign in with <FcGoogle />
-      </Button>
-    </Fieldset.Root>
+        <Button type="submit">Sign in</Button>
+        <Button>
+          Sign in with <FcGoogle />
+        </Button>
+
+        <Link to="/sign-up">Create an account</Link>
+      </Fieldset.Root>
+    </Box>
   );
 };
 
--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,28 +1,11 @@
-import { Box, Heading } from '@chakra-ui/react';
-import { useColorModeValue } from '../ui/color-mode';
+import { Flex } from '@chakra-ui/react';
 import LoginForm from '../forms/LoginForm';
-import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
-    <Box
-      maxW="md"
-      minW="sm"
-      mx="auto"
-      mt={16}
-      p={8}
-      borderWidth={1}
-      borderRadius="lg"
-      bg={useColorModeValue('white', 'gray.800')}
-      boxShadow="md"
-    >
-      <Heading mb={6} textAlign="center" size="lg">
-        Sign in to your account
-      </Heading>
+    <Flex direction="column" p={4} flex="1" justifyContent="center">
       <LoginForm />
-
-      <Link to="/sign-up">Create an account</Link>
-    </Box>
+    </Flex>
   );
 };
 

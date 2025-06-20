1e949c59e56a6642628f92e78012eda9b16141ec
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-06-19 21:21:08 -0400

    prisma seed

---
+++ server/src/scripts/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '../../generated/prisma';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: 1,
+      name: 'Leslie S Fernandez',
+      username: 'leslieshanell',
+      password: '$2b$10$61/MvArRA5HhYSXvU1jL6O5Kt3/i8yGRNKiC2oEAItapzK7obTbmm',
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      id: 2,
+      name: 'Liomard J Mesa',
+      username: 'Ljaquel01',
+      password: '$2b$10$bRX0Zj0FoJffnncDwPFc6eUfTtvn33tC7SgbEcdd6m1Jb4z/toM/G',
+    },
+  });
+
+  await prisma.folder.create({
+    data: {
+      id: 1,
+      ownerId: 1,
+      name: "Leslie's First Folder",
+    },
+  });
+
+  await prisma.folder.create({
+    data: {
+      id: 2,
+      ownerId: 2,
+      name: `LJ's First Folder`,
+    },
+  });
+
+  // await prisma.file.create({
+  //   data: {
+  //     id: 1,
+  //     ownerId: 1,
+  //     name: 'locrio.jpeg',
+  //     bucket: 'uploads',
+  //     mimeType: 'image/jpeg',
+  //     path: '1/home/locrio.jpeg',
+  //     size: 339806,
+  //     isFavorite: false,
+  //   },
+  // });
+}
+
+main()
+  .then(() => {
+    console.log('Seeding finished.');
+    return prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error(e);
+    return prisma.$disconnect().finally(() => process.exit(1));
+  });

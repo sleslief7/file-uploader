5dfebca7867311cfe68b7393ee5120da095e1c1c
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-06-27 15:49:15 -0400

    react-hook-form and zod

--- client/src/components/forms/LoginForm.tsx
+++ client/src/components/forms/LoginForm.tsx
@@ -1,45 +1,65 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import { Button, Field, Fieldset, Input } from '@chakra-ui/react';
+import { Button, Field, Fieldset, Input, Text } from '@chakra-ui/react';
 import useLogin from '@/hooks/useLogin';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+import * as z from 'zod/v4';
+
+const schema = z.object({
+  username: z.string().trim().min(3),
+  password: z.string().min(4),
+});
+
+type FormFields = z.infer<typeof schema>;
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const { mutate: login } = useLogin();
 
-  const handleSignin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login({ username, password });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const handleLogin: SubmitHandler<FormFields> = async (data) => {
+    await login(data);
   };
 
   return (
-    <form onSubmit={handleSignin}>
+    <form onSubmit={handleSubmit(handleLogin)}>
       <Fieldset.Root size="lg" maxW="md">
         <Fieldset.Legend fontSize="xl">Sign in to your account</Fieldset.Legend>
         <Field.Root>
           <Field.Label id="username">Username: </Field.Label>
-          <Input
-            placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <Input placeholder="Enter your username" {...register('username')} />
+          {errors.username && (
+            <Text color={'fg.error'}>{errors.username.message}</Text>
+          )}
         </Field.Root>
-
         <Field.Root>
           <Field.Label id="password">Password: </Field.Label>
           <Input
             placeholder="Enter your password"
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register('password')}
           />
+          {errors.password && (
+            <Text color={'fg.error'}>{errors.password.message}</Text>
+          )}
         </Field.Root>
 
-        <Button type="submit">Sign in</Button>
+        <Button type="submit">
+          {isSubmitting ? 'Logging in...' : 'Login'}
+        </Button>
         <Button>
-          Sign in with <FcGoogle />
+          Login with <FcGoogle />
         </Button>
 
         <Link to="/sign-up">Create an account</Link>
--- client/src/components/forms/SignupForm.tsx
+++ client/src/components/forms/SignupForm.tsx
@@ -1,23 +1,38 @@
 import { FcGoogle } from 'react-icons/fc';
-import { Button, Field, Fieldset, Input } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Button, Field, Fieldset, Input, Text } from '@chakra-ui/react';
+
 import { useNavigate } from 'react-router-dom';
 import useSignup from '@/hooks/useSignup';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+import * as z from 'zod/v4';
+
+const schema = z.object({
+  name: z.string().trim().min(3),
+  username: z.string().trim().min(3),
+  password: z.string().min(4),
+});
+
+type FormFields = z.infer<typeof schema>;
 
 const SignupForm = () => {
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const { mutate: signup } = useSignup();
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>({
+    defaultValues: {
+      name: '',
+      username: '',
+      password: '',
+    },
+    resolver: zodResolver(schema),
+  });
 
-  const handleSignup = () => {
-    const user = {
-      name,
-      username,
-      password,
-    };
-    signup(user);
+  const handleSignup: SubmitHandler<FormFields> = async (data) => {
+    await signup(data);
     navigate('/');
   };
   return (
@@ -26,33 +41,33 @@ const SignupForm = () => {
         <Fieldset.Legend fontSize="xl">Create an account</Fieldset.Legend>
         <Field.Root>
           <Field.Label id="name">Name: </Field.Label>
-          <Input
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <Input placeholder="Enter your name" {...register('name')} />
+          {errors.name && <Text color={'fg.error'}>{errors.name.message}</Text>}
         </Field.Root>
 
         <Field.Root>
           <Field.Label id="username">Username: </Field.Label>
-          <Input
-            placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <Input placeholder="Enter your username" {...register('username')} />
+          {errors.username && (
+            <Text color={'fg.error'}>{errors.username.message}</Text>
+          )}
         </Field.Root>
 
         <Field.Root>
           <Field.Label id="password">Password: </Field.Label>
           <Input
             placeholder="Enter your password"
-            value={password}
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            {...register('password')}
           />
+          {errors.password && (
+            <Text color={'fg.error'}>{errors.password.message}</Text>
+          )}
         </Field.Root>
 
-        <Button onClick={handleSignup}>Create account</Button>
+        <Button type="submit" onClick={handleSubmit(handleSignup)}>
+          Create account
+        </Button>
         <Button>
           Sign up with <FcGoogle />
         </Button>

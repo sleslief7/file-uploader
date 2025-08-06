1a9c38c0d3b0a2dca13829c3802be04bbfe5c31a
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-08-06 14:38:45 -0400

    useFolderIdParam bug fix

--- client/src/hooks/useFolderIdParam.tsx
+++ client/src/hooks/useFolderIdParam.tsx
@@ -2,11 +2,11 @@ import { useParams } from 'react-router-dom';
 
 const useFolderIdParam = (): number | null => {
   const { folderId: folderIdParam } = useParams();
-  const isHomeOrNull = folderIdParam === 'home' || folderIdParam === 'null';
+  const isHomeOrNull = folderIdParam === 'home' || !folderIdParam;
 
   if (isHomeOrNull) return null;
 
-  if (!isNaN(Number(folderIdParam))) throw new Error('Invalid folder id');
+  if (isNaN(Number(folderIdParam))) throw new Error('Invalid folder id');
 
   return Number(folderIdParam);
 };

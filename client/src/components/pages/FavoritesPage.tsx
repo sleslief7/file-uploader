0b12719c416c996b1ab086220976d690deb74141
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-09-07 22:28:44 -0400

    layout

--- client/src/components/pages/FavoritesPage.tsx
+++ client/src/components/pages/FavoritesPage.tsx
@@ -1,10 +1,12 @@
-import { Heading, Stack } from '@chakra-ui/react';
+import { Text, Stack } from '@chakra-ui/react';
 import ItemsTable from '../table/ItemsTable';
 
 const FavoritesPage = () => {
   return (
-    <Stack>
-      <Heading size='3xl'>Favorites</Heading>
+    <Stack gap={0}>
+      <Text textStyle='md' p={2} my={1.5}>
+        Favorites
+      </Text>
       <ItemsTable />
     </Stack>
   );
